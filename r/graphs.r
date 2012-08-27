5f3bdd6bb9ebb86e307e6ae1366e3beedd963f76
jaredlander <info@jaredlander.com>
2012-08-27 11:24:08 -0400

    Graphs since 1960.

--- r/graphs.r
+++ r/graphs.r
@@ -1,4 +1,8 @@
 # some plots
 require(ggplot2)
 
-ggplot(baseball, aes(x=Year, y=Yankees)) + geom_point()
+yanks <- ggplot(baseball[which(baseball$Year >= 1960), ], aes(x=Year, y=Yankees)) + opts(axis.text.x=theme_text(angle=90))
+yanks + geom_point(aes(colour=Party))
+
+yanksAll <- ggplot(baseball, aes(x=Year, y=Yankees)) + opts(axis.text.x=theme_text(angle=90))
+yanksAll + geom_point(aes(colour=Party))
